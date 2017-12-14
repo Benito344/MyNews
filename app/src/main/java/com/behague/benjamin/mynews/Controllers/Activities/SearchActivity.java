2c5b35c131027072703824c39dba65eb6628e1e5
Benito <behague.benjamin@gmail.com>
2017-12-14 11:31:40 +0100

    Modifications of SearchActivity Design

--- app/src/main/java/com/behague/benjamin/mynews/Controllers/Activities/SearchActivity.java
+++ app/src/main/java/com/behague/benjamin/mynews/Controllers/Activities/SearchActivity.java
@@ -1,7 +1,9 @@
 package com.behague.benjamin.mynews.Controllers.Activities;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.behague.benjamin.mynews.R;
 
@@ -11,5 +13,17 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_notifications);
+        this.configureToolbar();
+    }
+
+    private void configureToolbar(){
+        //Get the toolbar (Serialise)
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //Set the toolbar
+        setSupportActionBar(toolbar);
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
